09bc5047d51d7a37d024ee724ed200f57ab9d79c
Daniel Nögel <dn@shopware.de>
2014-06-12 18:09:27 +0200

    Set the output writer in the main application instead of doing it in "BaseCommand"

--- src/Application.php
+++ src/Application.php
@@ -4,6 +4,7 @@ namespace ShopwareCli;
 
 use Composer\Autoload\ClassLoader;
 use ShopwareCli\Application\DependencyInjection;
+use ShopwareCli\OutputWriter\WrappedOutputWriter;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -41,13 +42,25 @@ class Application extends \Symfony\Component\Console\Application
      */
     public function doRun(InputInterface $input, OutputInterface $output)
     {
-        $this->container = DependencyInjection::createContainer();
-        $this->container->set('autoloader', $this->loader);
+        $this->setupContainer($output);
 
         $this->container->get('plugin_manager')->init();
 
         $this->addCommands($this->container->get('command_manager')->getCommands());
 
         return parent::doRun($input, $output);
+    }
+
+    /**
+     * Creates the container and sets some services which are only synthetic in the container
+     *
+     * @param OutputInterface $output
+     */
+    protected function setupContainer(OutputInterface $output)
+    {
+        $this->container = DependencyInjection::createContainer();
+
+        $this->container->set('autoloader', $this->loader);
+        $this->container->set('output_writer', new WrappedOutputWriter(array($output, 'writeln')));
     }
 }
