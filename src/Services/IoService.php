d348572c7794395bd79b1f2226c320931fdd773e
Daniel Nögel <dn@shopware.com>
2014-06-13 13:38:03 +0200

    Add verbosity getters

--- src/Services/IoService.php
+++ src/Services/IoService.php
@@ -36,6 +36,55 @@ class IoService
     }
 
     /**
+     * Returns true if the input is interactive
+     *
+     * @return bool
+     */
+    public function isInteractive()
+    {
+        return $this->input->isInteractive();
+    }
+
+    /**
+     * Returns true if output is quiet
+     *
+     * @return bool
+     */
+    public function isQuiet()
+    {
+        return $this->output->getVerbosity() === OutputInterface::VERBOSITY_QUIET;
+    }
+
+    /**
+     * Return true if output is verbose (or more verbose)
+     *
+     * @return bool
+     */
+    public function isVerbose()
+    {
+        return $this->output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE;
+    }
+
+    /**
+     * Return true if output is ver verbose (or debug)
+     * @return bool
+     */
+    public function isVeryVerbose()
+    {
+        return $this->output->getVerbosity() >= OutputInterface::VERBOSITY_VERY_VERBOSE;
+    }
+
+    /**
+     * Return true if output is debug
+     *
+     * @return bool
+     */
+    public function isDebug()
+    {
+        return $this->output->getVerbosity() >= OutputInterface::VERBOSITY_DEBUG;
+    }
+
+    /**
      * Write a message to STDOUT
      *
      * @param $message
