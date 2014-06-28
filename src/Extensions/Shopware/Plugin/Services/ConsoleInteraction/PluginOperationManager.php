005629f8ef4d60b61d95605a02569280e958986b
Benjamin Cremer <bc@shopware.com>
2014-06-29 01:06:56 +0200

    Refactor PluginsOperationManager.

--- src/Extensions/Shopware/Plugin/Services/ConsoleInteraction/PluginOperationManager.php
+++ src/Extensions/Shopware/Plugin/Services/ConsoleInteraction/PluginOperationManager.php
@@ -5,9 +5,12 @@ namespace Shopware\Plugin\Services\ConsoleInteraction;
 use Shopware\Plugin\Services\PluginProvider;
 use ShopwareCli\Services\IoService;
 use Shopware\Plugin\Struct\Plugin;
+use ShopwareCli\Utilities;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Handles operations on plugins. You can either search for a single plugin, force the user to select one if multiple
+ * Handles operations on plugins.
+ * You can either search for a single plugin, force the user to select one if multiple
  * plugins where found and then operate on that plugin (searchAndOperate) or run the plugin prompt until the user
  * selects 'all' or 'exit' (operationLoop).
  *
@@ -18,16 +21,43 @@ use Shopware\Plugin\Struct\Plugin;
  */
 class PluginOperationManager
 {
-    protected $pluginProvider;
-    protected $output;
-    protected $pluginSelector;
     /**
-     * @var \ShopwareCli\Services\IoService
+     * @var PluginProvider
+     */
+    protected $pluginProvider;
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * @var Utilities
+     */
+    private $utilities;
+
+    /**
+     * @var PluginInputVerificator
+     */
+    protected $pluginSelector;
+
+    /**
+     * @var IoService
      */
     private $ioService;
 
-    public function __construct(PluginProvider $pluginProvider, PluginInputVerificator $pluginSelector, IoService $ioService, $utilities)
-    {
+    /**
+     * @param PluginProvider         $pluginProvider
+     * @param PluginInputVerificator $pluginSelector
+     * @param IoService              $ioService
+     * @param Utilities              $utilities
+     */
+    public function __construct(
+        PluginProvider $pluginProvider,
+        PluginInputVerificator $pluginSelector,
+        IoService $ioService,
+        Utilities $utilities
+    ) {
         $this->pluginProvider = $pluginProvider;
         $this->pluginSelector = $pluginSelector;
         $this->utilities = $utilities;
@@ -38,39 +68,55 @@ class PluginOperationManager
      * Search the plugin provider by $name and operate on the matching plugin. If multiple plugins are found
      * the users is asked for a selection. If no plugin was found, a corresponding message is printed
      *
-     * @param $names
-     * @param $callback
-     * @param $params
+     * @param string[] $names
+     * @param callable $callback
+     * @param array    $params
      */
     public function searchAndOperate($names, $callback, $params)
     {
+
         foreach ($names as $name) {
             $plugins = $this->pluginProvider->getPluginByName($name);
             $count = count($plugins);
-            if ($count == 1) {
-                $this->ioService->writeln("\nWill now process '<comment>{$name}</comment>'");
-                $callback($plugins[0], $params);
-            } elseif ($count > 1) {
-                $response = $this->pluginSelector->selectPlugin($plugins, array('all'));
-                if ($response instanceof Plugin) {
-                    $callback($response, $params);
-                } else {
-                    foreach ($plugins as $plugin) {
-                        $callback($plugin, $params);
-                    }
-                }
-            } elseif ($count == 0) {
+
+            if ($count == 0) {
                 $this->ioService->writeln("\n<error>Could not find a plugin named '{$name}'</error>");
+                return;
+            }
+
+            $this->ioService->writeln("\nWill now process '<comment>{$name}</comment>'");
+
+            if ($count == 1) {
+                $this->executeMethodCallback($plugins[0], $callback, $params);
+                return;
+            }
+
+            $response = $this->pluginSelector->selectPlugin($plugins, array('all'));
+            if ($response instanceof Plugin) {
+                $plugins = array($response);
+            }
+
+            foreach ($plugins as $plugin) {
+                $this->executeMethodCallback($plugin, $callback, $params);
             }
         }
+    }
 
+    /**
+     * @param object   $subject
+     * @param callable $callback
+     * @param array    $params
+     */
+    private function executeMethodCallback($subject, $callback, $params)
+    {
+        $callback($subject, $params);
     }
 
     /**
      * Show the plugin list to the user, until "all" or "exit" was entered
      *
-     * @param $callback
-     * @param $params
+     * @param callable $callback
+     * @param array    $params
      */
     public function operationLoop($callback, $params)
     {
@@ -80,18 +126,20 @@ class PluginOperationManager
             if ($response == 'exit') {
                 return;
             }
-            if ($response instanceof \Shopware\Plugin\Struct\Plugin) {
-                $this->utilities->cls();
-                $callback($response, $params);
-                $this->ioService->ask("\n<error>Done, hit enter to continue.</error>");
-                $this->utilities->cls();
 
-            } elseif ($response == 'all') {
+            if ($response == 'all') {
                 foreach ($plugins as $plugin) {
                     $callback($plugin, $params);
                 }
 
                 return;
+            }
+
+            if ($response instanceof Plugin) {
+                $this->utilities->cls();
+                $callback($response, $params);
+                $this->ioService->ask("\n<error>Done, hit enter to continue.</error>");
+                $this->utilities->cls();
             }
         }
     }
