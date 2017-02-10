38faf4b9be4ce703e9d80224b4e01fa3628dbbc8
Michael Telgmann <m.telgmann@shopware.com>
2017-02-10 10:07:20 +0100

    Fix checkout of plugins

--- src/Extensions/Shopware/Plugin/Services/Install.php
+++ src/Extensions/Shopware/Plugin/Services/Install.php
@@ -7,18 +7,15 @@ use ShopwareCli\Services\IoService;
 use ShopwareCli\Services\ProcessExecutor;
 
 /**
- * Checks out a given plugin, activates it and adds it to the phpstorm vcs.xml
- *
- * Class Install
- * @package Shopware\Plugin\Services
+ * Checks out a given plugin, activates it, adds it to the phpstorm vcs.xml, installs git hook
  */
 class Install
 {
     /**
      * @var Checkout
      */
-    protected $checkout
-    ;
+    protected $checkout;
+
     /**
      * @var IoService
      */
@@ -53,8 +50,7 @@ class Install
         $installationPath = $shopwarePath . '/engine/Shopware/Plugins/Local/';
         $pluginPath = $installationPath . '/' . $plugin->module . '/' . $plugin->name;
 
-        $isNewStructure =  $this->checkForNewStructure($pluginPath);
-        if ($isNewStructure) {
+        if (!$plugin->module) {
             $installationPath = $shopwarePath . '/custom/plugins/';
             $pluginPath = $installationPath . '/' . $plugin->name;
         }
@@ -71,29 +67,15 @@ class Install
             $this->ioService->writeln(exec($shopwarePath . '/bin/console sw:plugin:install --activate ' . $plugin->name));
         }
 
-        $this->addPluginVcsMapping($plugin, $shopwarePath, $isNewStructure);
+        $this->addPluginVcsMapping($plugin, $shopwarePath);
         $this->installGitHook($pluginPath);
-    }
-
-    /**
-     * @param string $pluginPath
-     * @return bool
-     */
-    private function checkForNewStructure($pluginPath)
-    {
-        if (file_exists($pluginPath . '/Bootstrap.php')) {
-            return false;
-        }
-
-        return true;
     }
 
     /**
      * @param Plugin $plugin
      * @param string $shopwarePath
-     * @param bool $isNewStructure
      */
-    private function addPluginVcsMapping(Plugin $plugin, $shopwarePath, $isNewStructure)
+    private function addPluginVcsMapping(Plugin $plugin, $shopwarePath)
     {
         $vcsMappingFile = $shopwarePath . '/.idea/vcs.xml';
         $pluginDestPath = $plugin->module . '/' . $plugin->name;
@@ -104,10 +86,16 @@ class Install
 
         $mapping = file_get_contents($vcsMappingFile);
         $xml = new \SimpleXMLElement($mapping);
+        foreach ($xml->component->mapping as $mapping) {
+            // if already mapped, return
+            if (strpos($this->normalize($mapping['directory']), $this->normalize($pluginDestPath)) !== false) {
+                return;
+            }
+        }
 
         $mappingDirectory = '$PROJECT_DIR$/engine/Shopware/Plugins/Local/' . $pluginDestPath;
 
-        if ($isNewStructure) {
+        if (!$plugin->module) {
             $mappingDirectory = '$PROJECT_DIR$/custom/plugins/' . $plugin->name;
         }
 
@@ -117,6 +105,17 @@ class Install
         $newMapping->addAttribute('directory', $mappingDirectory);
 
         $xml->asXML($vcsMappingFile);
+    }
+
+    /**
+     * Normalize directory strings to make them comparable
+     *
+     * @param $string
+     * @return string
+     */
+    private function normalize($string)
+    {
+        return strtolower(str_replace(['/', '\\'], '-', $string));
     }
 
     /**
